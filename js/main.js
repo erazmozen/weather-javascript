0517ff7736e37d21fda48edecd9d80cc6125bfa0
erazmozen <erazmozen@protonmail.com>
2023-03-06 13:44:02 +0100

    Add a function to get city data from search

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 console.log("started");
+
+const root = document.getElementById("root");
+
+const weather = {
+  fetchCity: async function (city) {
+    await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${city}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  },
+};
+
+const cityInput = document.getElementById("city-input");
+cityInput.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  if (e.target.value.length < 3) return;
+  weather.fetchCity(e.target.value);
+});
